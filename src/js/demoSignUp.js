712c6b66a4f8bada71adc270c0ff07417208af10
fleshren <50149975+fleshren@users.noreply.github.com>
2020-11-16 22:23:02 -0800

    Access PIN & Deletion Added
    This version now contains: a numbered list, access codes for students/TAs, and deletion of queue members by the TA only (modAccess).

--- src/js/demoSignUp.js
+++ src/js/demoSignUp.js
@@ -1,15 +1,69 @@
-const demoInputElement = document.querySelector('input:first-child');
+const demoInputElement = document.getElementById('names');
 const demoParent = document.querySelector('.demoParent');
+const pinInputElement = document.getElementById('pins');
+const pinSubmitButton = document.getElementById('pinSubmit');
+//let counter = 0;
 
+const pinTA = 95126;
+const pinStudent = 95050;
+let pinEntered = false;
+let modAccess = false;
+
+const nameSubmission = (event) => {
+  if(pinEntered == true){
+    //console.log("inside pinEntered true")
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    const span = document.createElement('span');
+    li.classList.add('li');
+    //li.setAttribute("id", counter);
+    //counter++;
+    span.textContent = demoInputElement.value;
+    button.classList.add('close');
+    button.setAttribute('type', 'button');
+    button.addEventListener('click', handleDeleteButton);
+    button.innerHTML = '<span aria-hidden="true">&times;</span>';
+    li.appendChild(span);
+    li.appendChild(button);
+    demoInputElement.value = "";
+    demoParent.appendChild(li);
+    demoInputElement.removeAttribute('disabled'); //idk if this is necessary
+  }else{
+    demoInputElement.value = "";
+    window.alert("Need Access Code");
+  }
+}
+const pinSubmission = (event) => {
+  pin = pinInputElement.value;
+  if(pin == pinTA){
+    //console.log("ta here");
+    pinEntered = true;
+    modAccess = true;
+    pinInputElement.value = "TA Access";
+    pinInputElement.style.backgroundColor = "#46a368";
+    pinSubmitButton.disabled = true;
+    pinInputElement.disabled = true;
+  }else if(pin == pinStudent){
+    //console.log("student here");
+    pinEntered = true;
+    pinInputElement.value = "Student Access";
+    pinInputElement.style.backgroundColor = "#34aeeb";
+    pinSubmitButton.disabled = true;
+    pinInputElement.disabled = true;
+  }else{
+    window.alert("Bad Access Code. Try Again Loser.");
+  }
+  //pinInputElement.value = "";
+}
+const handleDeleteButton = (event) => {
+  console.log(event);
+  if(modAccess == true){
+    event.path[2].remove();
+  }else {
+    window.alert("You're not a TA, loser");
+  }
+}
 const main = () => {
-    document.querySelector('input[value="Submit"]').addEventListener('click', () => {
-        const el = document.createElement('li');
-        el.classList.add('li');
-        el.textContent = demoInputElement.value;
-        demoInputElement.value = "";
-        demoParent.appendChild(el);
-        demoInputElement.removeAttribute('disabled');
-    })
 
 }
 

670900e54d3b5fefdc082ad86420cdcd69d99f1d
fleshren <50149975+fleshren@users.noreply.github.com>
2020-11-17 21:24:50 -0800

    The queue page is now for demo and questions queues
    - added container and entry field for questions queue
    - changed navbar to say Queue instead of Demo Queue
    - changed the pinStudent to an array of pins so each student can have a unique pin

--- src/js/demoSignUp.js
+++ src/js/demoSignUp.js
@@ -1,23 +1,23 @@
 const demoInputElement = document.getElementById('names');
+const questionInputElement = document.getElementById('qnames');
 const demoParent = document.querySelector('.demoParent');
+const questionParent = document.querySelector('.questionParent');
 const pinInputElement = document.getElementById('pins');
 const pinSubmitButton = document.getElementById('pinSubmit');
-//let counter = 0;
 
 const pinTA = 95126;
-const pinStudent = 95050;
+const pinStudent = [62221, 95050, 12345];
 let pinEntered = false;
 let modAccess = false;
 
-const nameSubmission = (event) => {
+//when a user submits a name to demo queue
+const demoSubmission = (event) => {
+  console.log(event);
   if(pinEntered == true){
-    //console.log("inside pinEntered true")
     const li = document.createElement('li');
     const button = document.createElement('button');
     const span = document.createElement('span');
     li.classList.add('li');
-    //li.setAttribute("id", counter);
-    //counter++;
     span.textContent = demoInputElement.value;
     button.classList.add('close');
     button.setAttribute('type', 'button');
@@ -29,42 +29,67 @@ const nameSubmission = (event) => {
     demoParent.appendChild(li);
     demoInputElement.removeAttribute('disabled'); //idk if this is necessary
   }else{
-    demoInputElement.value = "";
-    window.alert("Need Access Code");
+    //demoInputElement.value = "";
+    window.alert("Need Access Code!");
   }
 }
+
+//when a user submits a name to question queue
+const questionSubmission = (event) => {
+  console.log(event);
+  if(pinEntered == true){
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    const span = document.createElement('span');
+    li.classList.add('li');
+    span.textContent = questionInputElement.value;
+    button.classList.add('close');
+    button.setAttribute('type', 'button');
+    button.addEventListener('click', handleDeleteButton);
+    button.innerHTML = '<span aria-hidden="true">&times;</span>';
+    li.appendChild(span);
+    li.appendChild(button);
+    questionInputElement.value = "";
+    questionParent.appendChild(li);
+    questionInputElement.removeAttribute('disabled'); //idk if this is necessary
+  }else{
+    //questionInputElement.value = "";
+    window.alert("Need Access Code!");
+  }
+}
+
+//when a user submits a PIN
 const pinSubmission = (event) => {
   pin = pinInputElement.value;
   if(pin == pinTA){
-    //console.log("ta here");
     pinEntered = true;
     modAccess = true;
     pinInputElement.value = "TA Access";
     pinInputElement.style.backgroundColor = "#46a368";
     pinSubmitButton.disabled = true;
     pinInputElement.disabled = true;
-  }else if(pin == pinStudent){
-    //console.log("student here");
+  }else if(pinStudent.includes(parseInt(pin))){
     pinEntered = true;
     pinInputElement.value = "Student Access";
     pinInputElement.style.backgroundColor = "#34aeeb";
     pinSubmitButton.disabled = true;
     pinInputElement.disabled = true;
   }else{
-    window.alert("Bad Access Code. Try Again Loser.");
+    window.alert("Bad Access Code. Try Again.");
   }
-  //pinInputElement.value = "";
 }
+
+//when a user attempts to delete a user from either queue
 const handleDeleteButton = (event) => {
   console.log(event);
   if(modAccess == true){
     event.path[2].remove();
   }else {
-    window.alert("You're not a TA, loser");
+    window.alert("You're not a TA!");
   }
 }
+
 const main = () => {
 
 }
-
 main();

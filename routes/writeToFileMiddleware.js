3cfbd76f32ee160fb2234f964fb6d0fcc961f714
jfantab <johnlu1161@gmail.com>
2020-12-10 18:02:05 -0800

    Updated adding and getting links from database

--- routes/writeToFileMiddleware.js
+++ routes/writeToFileMiddleware.js
@@ -3,9 +3,6 @@ const fs = require('fs/promises')
 const writeToFileMiddleware = (req, res, next) => {
     next()
 
-    if(req.originalUrl.includes("links"))
-        fs.writeFile('./links.json', JSON.stringify(res.app.locals.links))
-
     fs.writeFile('./questionsPageStats.json', JSON.stringify(res.app.locals.questionsPageStats))
 }
 
